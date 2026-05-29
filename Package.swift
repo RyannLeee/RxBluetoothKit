654dfef9d2dfd592e56a6e2e0a878c16123853da
Ryan Lee <26449150+RyannLeee@users.noreply.github.com>
2026-05-29 12:02:26 +0800

    Change RxSwift dependency to major version 6.10.0
    Updated RxSwift dependency version to allow major updates.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "RxBluetoothKit_Airthings", targets: ["RxBluetoothKit_Airthings"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMinor(from: "6.9.0"))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.10.0"))
     ],
     targets: [
         .target(
@@ -30,9 +30,6 @@ let package = Package(
                 ],
                 sources: [
                     "Source"
-                ],
-                swiftSettings: [
-                    .enableExperimentalFeature("StrictConcurrency")
                 ]
             )
     ]
